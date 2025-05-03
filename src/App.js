95ba6405869bc5b4f158568c24f6bd4efcedc13f
BothSann <boththann76@gmail.com>
2025-05-03 13:33:57 +0700

    Refactor App component to manage loading state and fetch questions data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+
+const initialStates = {
+  questions: [],
+  // Could be "loading", "error", "ready". "active", "finished"
+  status: "loading",
+};
+function reducer(states, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...states,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...states,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is unknown");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialStates);
+  const numQuestions = questions.length;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataRecieved", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
